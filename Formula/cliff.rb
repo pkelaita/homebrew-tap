8e7b397069f4b191b8a0505e8cb8e688bf06fe9e
Pierce Kelaita <pierce@kelaita.com>
2025-06-21 16:25:09 -0700

    Update Cliff to v0.5.3

--- Formula/cliff.rb
+++ Formula/cliff.rb
@@ -3,8 +3,8 @@ class Cliff < Formula
 
   desc "Generate Unix commands in the terminal"
   homepage "https://github.com/pkelaita/cliff"
-  url "https://files.pythonhosted.org/packages/b2/8f/c9a370a2c4ef947d479d0ef05e88707bb41d904d51361d8aef9f93df9e58/cliff_cli-0.5.2.tar.gz"
-  sha256 "7bbf33e10590f3e655604f0e7bcf9a2ba471b679469f0d8809bb5cde6c7d1260"
+  url "https://files.pythonhosted.org/packages/c8/d0/51ef7917f578da2336dff382d4b054a9e416343486c48fcf3bfd67b78749/cliff_cli-0.5.3.tar.gz"
+  sha256 "b53f640de64d507538c436559278252af5712b6a45568355be293c319471e4eb"
   license "Apache-2.0"
 
   depends_on "python@3.13"
@@ -20,8 +20,8 @@ class Cliff < Formula
   end
 
   resource "certifi" do
-    url "https://files.pythonhosted.org/packages/e8/9e/c05b3920a3b7d20d3d3310465f50348e5b3694f4f88c6daf736eef3024c4/certifi-2025.4.26.tar.gz"
-    sha256 "0a816057ea3cdefcef70270d2c515e4506bbc954f417fa5ade2021213bb8f0c6"
+    url "https://files.pythonhosted.org/packages/73/f7/f14b46d4bcd21092d7d3ccef689615220d8a08fb25e564b65d20738e672e/certifi-2025.6.15.tar.gz"
+    sha256 "d747aa5a8b9bbbb1bb8c22bb13e22bd1f18e9796defa16bab421f7f7a317323b"
   end
 
   resource "h11" do
@@ -45,8 +45,8 @@ class Cliff < Formula
   end
 
   resource "l2m2" do
-    url "https://files.pythonhosted.org/packages/12/93/5c088d5acf6a097be0c65c566bb3e7e82c351d540c534ebc461525c2aebf/l2m2-0.0.53.tar.gz"
-    sha256 "e9a0bc1eb239e619b6764c4a45b4834fecee63ad0b49ff793373fcfc1715c156"
+    url "https://files.pythonhosted.org/packages/c4/4b/c0e6bc8cc62f780a7905c1b23027c9dd197cf3c79e53543a89199ad77b0d/l2m2-0.0.56.tar.gz"
+    sha256 "3a65e48fc7a7b4dbc5629cac4ab30d203fe75da30bd34ca09d64fba23c5ea091"
   end
 
   resource "markdown-it-py" do
@@ -60,8 +60,8 @@ class Cliff < Formula
   end
 
   resource "pydantic" do
-    url "https://files.pythonhosted.org/packages/77/ab/5250d56ad03884ab5efd07f734203943c8a8ab40d551e208af81d0257bf2/pydantic-2.11.4.tar.gz"
-    sha256 "32738d19d63a226a52eed76645a98ee07c1f410ee41d93b4afbfa85ed8111c2d"
+    url "https://files.pythonhosted.org/packages/00/dd/4325abf92c39ba8623b5af936ddb36ffcfe0beae70405d456ab1fb2f5b8c/pydantic-2.11.7.tar.gz"
+    sha256 "d989c3c6cb79469287b1569f7447a17848c998458d49ebe294e975b9baf0f0db"
   end
 
   resource "pydantic-core" do
@@ -70,8 +70,8 @@ class Cliff < Formula
   end
 
   resource "pygments" do
-    url "https://files.pythonhosted.org/packages/7c/2d/c3338d48ea6cc0feb8446d8e6937e1408088a72a39937982cc6111d17f84/pygments-2.19.1.tar.gz"
-    sha256 "61c16d2a8576dc0649d9f39e089b5f02bcd27fba10d8fb4dcc28173f7a45151f"
+    url "https://files.pythonhosted.org/packages/b0/77/a5b8c569bf593b0140bde72ea885a803b82086995367bf2037de0159d924/pygments-2.19.2.tar.gz"
+    sha256 "636cb2477cec7f8952536970bc533bc43743542f70392ae026374600add5b887"
   end
 
   resource "rich" do
@@ -85,13 +85,13 @@ class Cliff < Formula
   end
 
   resource "typing-extensions" do
-    url "https://files.pythonhosted.org/packages/f6/37/23083fcd6e35492953e8d2aaaa68b860eb422b34627b13f2ce3eb6106061/typing_extensions-4.13.2.tar.gz"
-    sha256 "e6c81219bd689f51865d9e372991c540bda33a0379d5573cddb9a3a23f7caaef"
+    url "https://files.pythonhosted.org/packages/d1/bc/51647cd02527e87d05cb083ccc402f93e441606ff1f01739a62c8ad09ba5/typing_extensions-4.14.0.tar.gz"
+    sha256 "8676b788e32f02ab42d9e7c61324048ae4c6d844a399eebace3d4979d75ceef4"
   end
 
   resource "typing-inspection" do
-    url "https://files.pythonhosted.org/packages/82/5c/e6082df02e215b846b4b8c0b887a64d7d08ffaba30605502639d44c06b82/typing_inspection-0.4.0.tar.gz"
-    sha256 "9765c87de36671694a67904bf2c96e395be9c6439bb6c87b5142569dcdd65122"
+    url "https://files.pythonhosted.org/packages/f8/b1/0c11f5058406b3af7609f121aaa6b609744687f1d158b3c3a5bf4cc94238/typing_inspection-0.4.1.tar.gz"
+    sha256 "6ae134cc0203c33377d43188d4064e9b357dba58cff3185f22924610e70a9d28"
   end
 
   def install
@@ -100,6 +100,6 @@ class Cliff < Formula
 
   test do
     output = shell_output("#{bin}/cliff --version")
-    assert_includes output, "0.5.2"
+    assert_includes output, "0.5.3"
   end
 end
